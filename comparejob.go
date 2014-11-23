bbb4f9e4ebae4c4606532f3c1faf0c857b3b95f5
rselph <dev@selph.net>
2014-11-22 23:15:06 -0800

    Intermediate work.

--- comparejob.go
+++ comparejob.go
@@ -2,8 +2,9 @@
 package main
 
 type compareJob struct {
-	f1, f2      *fileJob
-	description string
+	f1, f2 *fileJob
+	err    error
+	//description string
 }
 
 func Comparator(in1, in2 chan *fileJob, out chan *compareJob) {
@@ -25,15 +26,13 @@ func Comparator(in1, in2 chan *fileJob, out chan *compareJob) {
 
 		switch {
 		case c.f1.Err != nil || c.f2.Err != nil:
-			c.description = "ERROR: "
+			break
 
 		case !ignoreMTime && c.f1.Info.ModTime().UnixNano() > c.f2.Info.ModTime().UnixNano():
-			c.description = "ERROR: "
-			c.f1.Err = &myError{"File has been changed since backup was made."}
+			c.err = code_NEWER
 
 		case c.f1.Info.Size() != c.f2.Info.Size() || c.f1.Chksum != c.f2.Chksum:
-			c.description = "FAIL: "
-			c.f1.Err = &myError{"file content mismatch"}
+			c.err = code_BAD_SUM
 		}
 
 		out <- c
--- walker.go
+++ walker.go
@@ -26,8 +26,6 @@ var excludeDirs = []string{
 }
 var excludeDirRegexp []*regexp.Regexp
 
-var notEligible *myError = &myError{"File skipped"}
-
 func initWalkers() {
 	excludeFileRegexp = make([]*regexp.Regexp, len(excludeFiles))
 	for i, v := range excludeFiles {
@@ -40,87 +38,51 @@ func initWalkers() {
 	}
 }
 
-func isEligible(path string, info os.FileInfo) (err error) {
-	switch {
-	case info == nil:
-		err = notEligible
-
-	case info.Mode().IsRegular():
-		for _, re := range excludeFileRegexp {
-			if re.MatchString(info.Name()) {
-				//fmt.Println("Ingoring:", path)
-				return notEligible
-			}
-		}
-
-	case info.Mode().IsDir():
-		err = notEligible
-		for _, re := range excludeDirRegexp {
-			if re.MatchString(path) {
-				//fmt.Println("Ingoring Dir:", path)
-				return filepath.SkipDir
-			}
-		}
-
-	default:
-		err = notEligible
-	}
-
-	return
-}
-
 type fileActor func(node string, info os.FileInfo)
 
-func actionVisitor(node string, info os.FileInfo, err error, action fileActor) (returnError error) {
-	if err != nil {
+func visitFileWithAction(node string, info os.FileInfo, err error, action fileActor) error {
+	if err != nil || info == nil {
 		return nil
 	}
 
-	returnError = isEligible(node, info)
-	switch returnError {
-
-	case nil:
+	switch {
+	case info.Mode().IsRegular():
+		for _, re := range excludeFileRegexp {
+			if re.MatchString(info.Name()) {
+				return nil
+			}
+		}
 		action(node, info)
 
-	case filepath.SkipDir:
-		break
-
-	default:
-		returnError = nil
-
+	case info.Mode().IsDir():
+		for _, re := range excludeDirRegexp {
+			if re.MatchString(node) {
+				return filepath.SkipDir
+			}
+		}
 	}
 
-	return
+	return nil
 }
 
-func DualWalker(in1, in2 chan *fileJob, path1, path2 string) {
+func Walker(in1, in2 chan *fileJob, path1, path2 string) {
 	defer func() { in1 <- nil }()
-	defer func() { in2 <- nil }()
 
-	prefix_len := len(path1)
-
-	action := func(node string, info os.FileInfo) {
-		in1 <- &fileJob{node, info, nil, 0}
-		in2 <- &fileJob{filepath.Join(path2, node[prefix_len:]), nil, nil, 0}
+	var action fileActor
+	if in2 == nil {
+		action = func(node string, info os.FileInfo) {
+			in1 <- &fileJob{node, info, nil, 0}
+		}
+	} else {
+		prefix_len := len(path1)
+		action = func(node string, info os.FileInfo) {
+			in1 <- &fileJob{node, info, nil, 0}
+			in2 <- &fileJob{filepath.Join(path2, node[prefix_len:]), nil, nil, 0}
+		}
+		defer func() { in2 <- nil }()
 	}
 
-	fileVisitor := func(node string, info os.FileInfo, err error) error {
-		return actionVisitor(node, info, err, action)
-	}
-
-	filepath.Walk(path1, fileVisitor)
-}
-
-func Walker(in chan *fileJob, path string) {
-	defer func() { in <- nil }()
-
-	action := func(node string, info os.FileInfo) {
-		in <- &fileJob{node, info, nil, 0}
-	}
-
-	fileVisitor := func(node string, info os.FileInfo, err error) error {
-		return actionVisitor(node, info, err, action)
-	}
-
-	filepath.Walk(path, fileVisitor)
+	filepath.Walk(path1, func(node string, info os.FileInfo, err error) error {
+		return visitFileWithAction(node, info, err, action)
+	})
 }
